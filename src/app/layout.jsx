5f8c53984963934b6f72b299a695f485ad5d3ff2
Estefania <estefania.osses.v@gmail.com>
2024-03-26 01:12:55 -0300

    Add hotjar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,6 +23,22 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script src="https://kit.fontawesome.com/a790242b27.js"></Script>
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html:
+          `<!-- Hotjar Tracking Code for https://psm-opal.vercel.app/ -->
+          <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3920275,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
+          `
+          
+        }}></Script>
       </head>
       <body className={inter.className}>
         <AuthProvider session={session}>
