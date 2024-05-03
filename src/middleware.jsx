759711e95a5a8307f33c7267a639a4332a9a70b6
Estefania <estefania.osses.v@gmail.com>
2024-05-03 15:23:13 -0400

    Add protected routes

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,15 +1,36 @@
-import { sessionStatus } from "./utils/session";
+import axios from "axios";
+import { sessionStatus } from "./utils/auth";
 import { NextRequest, NextResponse } from "next/server";
 
-// const protectedRoutes = ["/middlewareside"];
+/* const protectedRoutes = [
+  "/dashboard", 
+  "/pacientes", 
+  "/pacientes/:path*", 
+  "/citas", 
+  "/citas/:path*", 
+  "/citas/agendarentrevista", 
+  "/citas/agendarcita", 
+  "/profesionales", 
+  "/profesionales/agregarprofesional", 
+  "/profesionales/:path",
+  "/blog",
+  "/blog/agregarblog",
+  "/blog/editar/:path",
+];
 
-// export default function middleware(req) {
-//   if(!sessionStatus && protectedRoutes.includes(req.nextUrl.pathname)) {
-//     const absoluteURL = new URL("/", req.nextUrl.origin);
-//     return NextResponse.redirect(absoluteURL.toString())
-//   } 
-// }
+export default function middleware(req) {
+  console.log('sessionStatus', sessionStatus);
+  if(!sessionStatus && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const absoluteURL = new URL("/", req.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString())
+  } 
+}
 
+axios('http://localhost:3000/api/auth/session')
+  .then(res => console.log('MIDDLE RES', res))
+  .catch(err => console.log('MIDDLE ERR', err))
+ */
+// ESTO ES LO QUE IBA
 export { default } from "next-auth/middleware"
 
 export const config = {matcher: [
@@ -18,7 +39,12 @@ export const config = {matcher: [
   "/pacientes/:path*", 
   "/citas", 
   "/citas/:path*", 
-  "/profesionales",
+  "/citas/agendarentrevista", 
+  "/citas/agendarcita", 
+  "/horarios",
+  "/horarios/agregarhorario/:path",
+  "/profesionales", 
+  "/profesionales/agregarprofesional", 
   "/profesionales/:path",
   "/blog",
   "/blog/agregarblog",
