56aa5ba92cc8857db8fddb516725b7b13175d2c2
Estefania <estefania.osses.v@gmail.com>
2024-05-13 08:06:45 -0400

    Fix keys for mapping data

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -10,31 +10,8 @@ import Select from "react-select";
 /* eslint-disable no-unused-vars */
 import { fetchBlog } from '@/services/BlogServices';
 
-import {
-  saludMental01, saludMental02, saludMental03
-}
-  from '../../../components/imagepath';
+import { blogs } from '@/utils/blogs';
 
-const blogs = [
-  {
-    id: '1',
-    titulo: 'Titulo 01',
-    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatem molestias eos. Ullam nihil quis inventore. Dolore, perspiciatis culpa quisquam, eligendi dolorum magnam, aliquid ipsam eos unde expedita eveniet tempore.',
-    imagen: saludMental01.src
-  },
-  {
-    id: '2',
-    titulo: 'Titulo 02',
-    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatem molestias eos. Ullam nihil quis inventore. Dolore, perspiciatis culpa quisquam, eligendi dolorum magnam, aliquid ipsam eos unde expedita eveniet tempore.',
-    imagen: saludMental02.src
-  },
-  {
-    id: '3',
-    titulo: 'Titulo 03',
-    texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatem molestias eos. Ullam nihil quis inventore. Dolore, perspiciatis culpa quisquam, eligendi dolorum magnam, aliquid ipsam eos unde expedita eveniet tempore.',
-    imagen: saludMental03.src
-  }
-]
 
 const Blogdetails = ({ params }) => {
   const [blog, setBlog] = useState({})
@@ -125,34 +102,8 @@ const Blogdetails = ({ params }) => {
                           />
                         </Link>
                       </div>
-                      <div className="blog-content">
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                          do eiusmod tempor incididunt ut labore et dolore magna
-                          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                          ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          Duis aute irure dolor in reprehenderit in voluptate velit
-                          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                          occaecat cupidatat non proident, sunt in culpa qui officia
-                          deserunt mollit anim id est laborum.
-                        </p>
-                        <p>
-                          Sed ut perspiciatis unde omnis iste natus error sit
-                          voluptatem accusantium doloremque laudantium, totam rem
-                          aperiam, eaque ipsa quae ab illo inventore veritatis et
-                          quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                          enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-                          aut fugit, sed quia consequuntur magni dolores eos qui
-                          ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-                          qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                          velit, sed quia non numquam eius modi tempora incidunt ut
-                          labore et dolore magnam aliquam quaerat voluptatem. Ut enim
-                          ad minima veniam, quis nostrum exercitationem ullam corporis
-                          suscipit laboriosam, nisi ut aliquid ex ea commodi
-                          consequatur? Quis autem vel eum iure reprehenderit qui in ea
-                          voluptate velit esse quam nihil molestiae consequatur, vel
-                          illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-                        </p>
+                      <div className="blog-content" dangerouslySetInnerHTML={{ __html: blog.texto }}>
+                        {/* {blog.texto} */}
                       </div>
                       {/* <div className="blog-share ">
                         <ul className="social-share nav">
