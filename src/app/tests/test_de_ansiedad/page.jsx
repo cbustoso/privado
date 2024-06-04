d0054b23d41ab6da0fb07dcdec469814a6c98a7b
Estefania <estefania.osses.v@gmail.com>
2024-06-04 14:56:42 -0400

    Add Fragment component

--- src/app/tests/test_de_ansiedad/page.jsx
+++ src/app/tests/test_de_ansiedad/page.jsx
@@ -9,6 +9,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { Fragment } from "react";
 
 const style = {
   position: 'absolute',
