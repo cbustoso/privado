5323af67fd76ea8dd4607ca30a1f4821b0b1de5f
Estefania <estefania.osses.v@gmail.com>
2024-07-10 14:38:41 -0400

    Add quienes somos section to home page

--- src/components/QuienesSomos.jsx
+++ src/components/QuienesSomos.jsx
@@ -12,12 +12,13 @@ import ReserveBtn from "./ReserveBtn";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { profesional01 } from "./imagepath";
 
-const ReservaTuHora = () => {
+const QuienesSomos = () => {
+
   const matches = useMediaQuery('(min-width:600px)');
 
 
   return (
-    <div className={`container col-12 col-lg-10 align-self-center p-0 ${matches && 'mb-5'}`} style={{ background: '#f1f1f1'}}>
+    <div className={`container col-12 col-lg-10 align-self-center p-0 ${matches && 'mb-5'}`} >
 
       {/* <TestCard test={slides} /> */}
 
@@ -28,7 +29,7 @@ const ReservaTuHora = () => {
           borderRadius: '32px',
           // padding: '24px 16px',
           textWrap: 'pretty',
-          margin: '0 auto'
+          margin: '0 auto',
         }}>
           <div className="row" style={{ margin: 0 }} >
             {
@@ -41,26 +42,9 @@ const ReservaTuHora = () => {
                     fontWeight: 400,
                     lineHeight: '32px',
                     borderRadius: '12px',
-                    marginTop: '20px'
+                    marginTop: '20px',
                   }}
                 >
-                  <Card
-                    className='col-12 col-lg-4'
-                    sx={{
-                      boxShadow: 0,
-                      display: 'inline-block',
-                      padding: '2em 0',
-                      bgcolor: '#f1f1f1'
-                    }}>
-                    <CardMedia
-                      component="img"
-                      height="240"
-                      width="240"
-                      image="https://github.com/Niennis/imagesudp/blob/main/profesional01.jpg?raw=true"
-                      sx={{ borderRadius: '8px' }}
-                    />
-                  </Card>
-
                   <Card
                     className='col-12 col-lg-8'
                     sx={{
@@ -72,7 +56,6 @@ const ReservaTuHora = () => {
                       justifyContent: 'space-between',
                       paddingBottom: '10px',
                       padding: '2em',
-                      bgcolor: '#f1f1f1'
                     }}>
                     <Typography
                       variant="body2"
@@ -84,7 +67,7 @@ const ReservaTuHora = () => {
                         lineHeight: '40px',
                         fontWeight: 700
                       }}>
-                      Reserva tu hora
+                      Quiénes somos
                     </Typography>
                     <Typography
                       variant="body2"
@@ -95,7 +78,7 @@ const ReservaTuHora = () => {
                         fontSize: '20px',
                         lineHeight: '28px'
                       }}>
-                      Si necesitas ayuda psicológica, reserva una hora con un psicólogo.
+                      El Departamento de Salud Mental Estudiantil (DSME) es un equipo cuya misión es contribuir al bienestar psicológico y emocional del estudiantado de la UDP.
                     </Typography>
                     <Typography
                       variant="body2"
@@ -106,14 +89,44 @@ const ReservaTuHora = () => {
                         fontSize: '20px',
                         lineHeight: '28px'
                       }}>
-                      Un psicólogo puede ayudarte a mejorar tu salud mental y emocional. Reservar una hora es fácil y cómodo.
+                      Nuestro modelo de intervención se basa en la Promoción, Prevención, Atención Grupal y Atención Clínica Individual; con la finalidad de prestar apoyo para enfrentar las diversas problemáticas psicosociales que se presentan en el transcurso de la vida estudiantil.
                     </Typography>
                     <CardActions sx={{ padding: '0px' }}>
                       <Typography size="medium" sx={{ padding: '0 5px', color: '#000' }}>
-                        <ReserveBtn text={'Reservar'} bgColor={'#FABB00'} color={'#000'} />
+                        <Link href={`/quienes-somos`}>
+                          <button
+                            className={`btn submit-form me-2 sailec-medium `}
+                            style={{
+                              width: '209px',
+                              height: '56px',
+                              backgroundColor: '#1abc9c',
+                              border: `1px solid #73cdcd`,
+                              borderRadius: '100px',
+                              color: '#fff',
+                              fontWeight: 500,
+                              fontSize: '16px',
+                            }}> Ver más + </button>
+                        </Link>
                       </Typography>
                     </CardActions>
                   </Card>
+
+                  <Card
+                    className='col-12 col-lg-4'
+                    sx={{
+                      boxShadow: 0,
+                      display: 'inline-block',
+                      padding: '2em 0',
+                    }}>
+                    <CardMedia
+                      component="img"
+                      height="240"
+                      width="240"
+                      image="https://github.com/Niennis/imagesudp/blob/main/profesional01.jpg?raw=true"
+                      sx={{ borderRadius: '8px' }}
+                    />
+                  </Card>
+
                 </div>
 
                 :
@@ -127,7 +140,7 @@ const ReservaTuHora = () => {
                     padding: 0,
                   }}
                 >
-                  <Box sx={{ bgcolor: '#99D6E9', fontFamily: 'sailec', lineHeight: '30px', }}>
+                  <Box sx={{ bgcolor: '#f1f1f1', fontFamily: 'sailec', lineHeight: '30px', }}>
                     <div className="media-body sailec" style={{ margin: '0 20px', padding: '10px 0', fontSize: '20px', fontWeight: 400, lineHeight: '28px' }}>
                       <Typography
                         variant="body2"
@@ -140,17 +153,29 @@ const ReservaTuHora = () => {
                           fontWeight: 700,
                           margin: '16px 0'
                         }}>
-                        Reserva tu hora
+                        Quiénes somos
                       </Typography>
 
                       <p className="media-body sailec" style={{ textWrap: 'pretty' }}>
-                        Si necesitas ayuda psicológica, reserva una hora con un psicólogo.
+                        El Departamento de Salud Mental Estudiantil (DSME) es un equipo cuya misión es contribuir al bienestar psicológico y emocional del estudiantado de la Universidad Diego Portales.
                       </p>
                       <p>
-                        Un psicólogo puede ayudarte a mejorar tu salud mental y emocional. Reservar una hora es fácil y cómodo.
+                        Nuestro modelo de intervención se basa en la Promoción, Prevención, Atención Grupal y Atención Clínica Individual; con la finalidad de prestar apoyo para enfrentar las diversas problemáticas psicosociales que se presentan en el transcurso de la vida estudiantil.
 
                       </p>
-                      <ReserveBtn text={'Reservar'} bgColor={'#FABB00'} color={'#000'} />
+                      <Link href={`/quienes-somos`}>
+                        <button
+                          className="btn submit-form me-2"
+                          style={{
+                            backgroundColor: 'transparent',
+                            border: '1px solid #000',
+                            borderRadius: '100px',
+                            color: '#000',
+                            width: '116px',
+                            height: '40px',
+                            margin: '8px'
+                          }}> Ver más + </button>
+                      </Link>
                     </div>
                   </Box>
                 </div>
@@ -162,4 +187,4 @@ const ReservaTuHora = () => {
   )
 }
 
-export default ReservaTuHora;
+export default QuienesSomos;
