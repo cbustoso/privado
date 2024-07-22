4a22d6be00332245da7c6d283e8e1397047afe34
Estefania <estefania.osses.v@gmail.com>
2024-07-22 17:26:59 -0400

    Change navbar, remove element from menu

--- src/app/quienes_somos/layout.jsx
+++ src/app/quienes_somos/layout.jsx
@@ -53,7 +53,7 @@ export default function QuienesSomosLayout({ children }) {
               {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button> */}
-
+{/* 
               <div  id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                   <li className="nav-item" >
@@ -92,7 +92,7 @@ export default function QuienesSomosLayout({ children }) {
                     </Link>
                   </li>
                 </ul>
-              </div>
+              </div> */}
             </nav>
             {children}
           </div>
